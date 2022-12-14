10ee23072800b905ab2cc7f42f92aad4b93c2221
sagar456-ops <sagarrimal87@gmail.com>
2022-12-14 21:46:56 +0545

    panel changed

--- adminpanel.php
+++ adminpanel.php
@@ -2,125 +2,188 @@
 include('database.php');
 session_start();
 ?>
-       <?php if ($_SESSION['role'] == 'manager' || $_SESSION['role']== 'super admin') {
-            ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php if ($_SESSION['role'] == 'manager' || $_SESSION['role'] == 'super admin') {
+?>
+      <!DOCTYPE html>
+      <html lang="en">
 
-<head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-      <!--for reloading-->
-      <meta http-equiv="Cache-control" content="no-cache">
-      <!--css for whole website-->
-      <link rel="stylesheet" href=".\css\style.css">
-      <!--css for responsive-->
-      <link rel="stylesheet" href=".\css\responsive.css">
-      <!--css for animations-->
-      <link rel="stylesheet" href=".\css\animate.css">
-      <!--link to use google fonts-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            rel="stylesheet">
-      <!--link to fonts-->
-      <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-      <!--for favicon-->
-      <link rel="icon" type="images/logo.png" href="images/logo.png">
-      <title>Admin-panel</title>
-
+      <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+            <!--for reloading-->
+            <meta http-equiv="Cache-control" content="no-cache">
+            <!--css for whole website-->
+            <link rel="stylesheet" href=".\css\style.css">
+            <!--css for responsive-->
+            <link rel="stylesheet" href=".\css\responsive.css">
+            <!--css for animations-->
+            <link rel="stylesheet" href=".\css\animate.css">
+            <!--link to use google fonts-->
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <!--link to fonts-->
+            <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+            <!--for favicon-->
+            <link rel="icon" type="images/logo.png" href="images/logo.png">
+            <title>Admin-panel</title>
 
 
 
-</head>
 
-<body>
-      <input type="checkbox" id="nav-toggle">
-      <?php include './admin/sidebar.php';?>
-      <div class="main-content">
-            <?php include './admin/header1.php';?>
-            <main>
-                  <div class="cards">
-                        <div class="card-single">
-                              <div>
-                                  <h1>54</h1>
-                                  <span>Customers</span>  
-                              </div>
-                              <div>
-                                    <span class="las la-users"></span>
-                              </div>
-                        </div>
-                        <div class="card-single">
-                              <div>
-                                  <h1>79</h1>
-                                  <span>Areas</span>  
-                              </div>
-                              <div>
-                                    <span class="las la-map-marker"></span>
-                              </div>
-                        </div>
-                        <div class="card-single">
-                              <div>
-                                  <h1>79</h1>
-                                  <span>Managers</span>  
-                              </div>
-                              <div>
-                                    <span class="las la-user-tie"></span>
-                              </div>
-                        </div>
-                        <div class="card-single">
-                              <div>
-                                  <h1>Rs. 10,000</h1>
-                                  <span>Income</span>  
-                              </div>
-                              <div>
-                                    <span class="lab la-google-wallet"></span>
-                              </div>
-                        </div>
+      </head>
 
-                  </div>
+      <body>
+            <input type="checkbox" id="nav-toggle">
+            <?php include './admin/sidebar.php'; ?>
+            <div class="main-content">
+                  <?php include './admin/header1.php'; ?>
+                  <main>
+                        <div class="cards">
+                              <div class="card-single">
+                                    <div>
+                                          <h1>
+                                                <?php
+                                                $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                                mysqli_select_db($con, 'sagar');
 
-                  <div class="recent-grid">
-                        <div class="projects">
-                              <div class="card">
-                                    <div class="card-header">
-                                          <h3>Recent added managers</h3>
-                                          <button> See all <span class="las al-arrow-right"></span></button>
-                                    </div> 
-                                    <div class="card-body">
-                                          <div class="table-responsive">
-                                                <table width="100%">
-                                                      <thead>
-                                                            <tr>  
-                                                                  <td>Manager Id</td>
-                                                                  <td> Manager Name </td>
-                                                                  <td> Location Area </td>
-                                                            </tr>
-                                                      </thead>
-                                                      <tbody>
-                                                            <tr>
-                                                                  <td>001</td>
-                                                                  <td> Sujen Shrestha </td>
-                                                                  <td> Gongabu </td>
-                                                            </tr>
-                                                      </tbody>
-                                                </table> 
-                                          </div>
-                                         
+                                                $result = mysqli_query($con, "SELECT COUNT(*) as total FROM usertable");
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                      echo $row['total'];
+                                                }
+                                                ?>
+                                          </h1>
+                                          <span>Customers</span>
+                                    </div>
+                                    <div>
+                                          <span class="las la-users"></span>
                                     </div>
                               </div>
-                              
+                              <div class="card-single">
+                                    <div>
+                                          <h1>
+                                                <?php
+                                                $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                                mysqli_select_db($con, 'sagar');
+
+                                                $result = mysqli_query($con, "SELECT COUNT(*) as total FROM area");
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                      echo $row['total'];
+                                                }
+                                                ?>
+
+
+
+
+                                          </h1>
+                                          <span>Areas</span>
+                                    </div>
+                                    <div>
+                                          <span class="las la-map-marker"></span>
+                                    </div>
+                              </div>
+                              <div class="card-single">
+                                    <div>
+                                          <h1>
+                                                <?php
+                                                $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                                mysqli_select_db($con, 'sagar');
+
+                                                $result = mysqli_query($con, "SELECT COUNT(*) as total FROM admintable WHERE role = 'manager'");
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                      echo $row['total'];
+                                                }
+                                                ?>
+                                          </h1>
+                                          <span>Managers</span>
+                                    </div>
+                                    <div>
+                                          <span class="las la-user-tie"></span>
+                                    </div>
+                              </div>
+                              <?php       if ($_SESSION['role'] == 'super admin') : ?>
+                              <div class="card-single">
+                                    <div>
+                                          <h1>
+                                                <?php
+                                                $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                                mysqli_select_db($con, 'sagar');
+
+                                                $result = mysqli_query($con, "SELECT SUM(amount) as total FROM payment GROUP BY amount");
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                      echo "Rs.";
+                                                      echo $row['total'];
+                                                }
+                                                ?>
+
+
+
+                                          </h1>
+                                          <span>Income</span>
+                                    </div>
+                                    <div>
+                                          <span class="lab la-google-wallet"></span>
+                                    </div>
+                              </div>
+<?php endif; ?>
                         </div>
-                  </div>
+<?php       if ($_SESSION['role'] == 'super admin') : ?>
+                        <div class="recent-grid">
+                              <div class="projects">
+                                    <div class="card">
+                                          <div class="card-header">
+                                                <h3>Recent added managers</h3>
+                                                <button> See all <span class="las al-arrow-right"></span></button>
+                                          </div>
+                                          <div class="card-body">
+                                                <div class="table-responsive">
+                                                      <?php
+                                               
+                                                      $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                                      mysqli_select_db($con, 'sagar');
+                                                      $result = mysqli_query($con, "SELECT * FROM admintables WHERE role = 'manager'");
+                                                      while ($row = mysqli_fetch_array($result)) {
+                                                            ?>
+                                                     
+                                                      <table width="100%">
+                                                            <thead>
+                                                                  <tr>
+                                                                        <td>Manager Id</td>
+                                                                        <td> Manager Name </td>
+                                                                        <td> Location Area </td>
+                                                                  </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                  <tr>
+                                                                        <td><?php echo $row['admin_id'];?></td>
+                                                                        <td><?php echo $row['name'];?></</td>
+                                                                        <td><?php echo $row['location'];?></</td>
+                                                                        
+                                                                  </tr>
+                                                            </tbody>
+                                                      </table>
+                                                      <?php } 
+                                                      endif; 
+                                                      ?>
+                                                </div>
 
-            </main>
-      </div>
-</body>
+                                          </div>
+                                    </div>
 
-</html>
-<?php } 
+                              </div>
+                        </div>
 
-else{
-       header('location:adminlogin.php');
+                  </main>
+            </div>
+      </body>
+
+      </html>
+<?php } else {
+      header('location:adminlogin.php');
 }
 
+?>
+
+
+<?php
+//$sql = "SELECT amount,name FROM `payment`INNER JOIN usertable ON payment.user_id = usertable.id";
 ?>
--- main.php
+++ main.php
@@ -18,8 +18,7 @@ session_start();
       <!--css for animations-->
       <link rel="stylesheet" href=".\css\animate.css">
       <!--link to use google fonts-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--link to fonts-->
       <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
       <!--for favicon-->
@@ -29,20 +28,21 @@ session_start();
 
 
 
+
 </head>
 
 <body>
       <input type="checkbox" id="nav-toggle">
-      <?php include './user/sidebar-user.php';?>
+      <?php include './user/sidebar-user.php'; ?>
       <div class="main-content">
-            <?php include './user/header-user.php';?>
+            <?php include './user/header-user.php'; ?>
             <main>
                   <div class="content homepage active">
                         <div class="cards">
                               <div class="card-single">
                                     <div>
-                                    <h1>Yes</h1>
-                                    <span>Schedule-today</span>  
+                                          <h1>Yes</h1>
+                                          <span>Schedule-today</span>
                                     </div>
                                     <div>
                                           <span class="las la-calendar-day"></span>
@@ -50,8 +50,8 @@ session_start();
                               </div>
                               <div class="card-single">
                                     <div>
-                                    <h1>Active</h1>
-                                    <span>Account-Status</span>  
+                                          <h1>Active</h1>
+                                          <span>Account-Status</span>
                                     </div>
                                     <div>
                                           <span class="las la-wallet"></span>
@@ -59,8 +59,8 @@ session_start();
                               </div>
                               <div class="card-single-time">
                                     <div>
-                                    <h1>3</h1>
-                                    <span>Time-Remaining</span>  
+                                          <h1>3</h1>
+                                          <span>Time-Remaining</span>
                                     </div>
                                     <div>
                                           <span class="las la-calendar-times"></span>
@@ -68,65 +68,65 @@ session_start();
                               </div>
                               <div class="card-single">
                                     <div>
-                                    <h1>Gongabu</h1>
-                                    <span>My-Area</span>  
+                                          <h1>Gongabu</h1>
+                                          <span>My-Area</span>
                                     </div>
                                     <div>
                                           <span class="las la-map-marker"></span>
                                     </div>
                               </div>
 
-                        </div>  
+                        </div>
                   </div>
                   <div class="content mappage in-active">
-                        <div id = "map" class="map-panel"></div>
-                        <script src = "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+                        <div id="map" class="map-panel"></div>
+                        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css"/>
+                        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
                         <script>
-
                               <?php
-                                    $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
-                                    mysqli_select_db($con, 'sagar');
+                              $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                              mysqli_select_db($con, 'sagar');
 
-                                    $result = mysqli_query($con, "SELECT * FROM locations");
-                                    while ($row = mysqli_fetch_array($result)) {
-                                          $lat_d = $row['latitude'];
-                                          $long_d = $row['longitude'];
-                                    }
+                              $result = mysqli_query($con, "SELECT * FROM locations");
+                              while ($row = mysqli_fetch_array($result)) {
+                                    $lat_d = $row['latitude'];
+                                    $long_d = $row['longitude'];
+                              }
 
-                                    $result = array(array('latitude' => $lat_d, 'longitude' => $long_d));
-                                    // Set the latitude and longitude coordinates of the location
-                                    $latitude = $lat_d;
-                                    $longitude = $long_d;
+                              $result = array(array('latitude' => $lat_d, 'longitude' => $long_d));
+                              // Set the latitude and longitude coordinates of the location
+                              $latitude = $lat_d;
+                              $longitude = $long_d;
                               ?>
                               const lat = <?php echo $latitude; ?>;
                               const long = <?php echo $longitude; ?>;;
                               // Creating map options
 
+
                               var mapOptions = {
                                     center: [lat, long],
                                     zoom: 13
                               }
-                              
+
                               // Creating a map object
                               var map = new L.map('map', mapOptions);
-                              
+
                               // Creating a Layer object
                               var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-                              
+
                               // Adding layer to the map
                               map.addLayer(layer);
-                              
+
                               const leafletMarkers = L.layerGroup([
 
 
-                              //adding marker
-                              <?php
+                                    //adding marker
+                                    <?php
 
-                                    $result = mysqli_query($con,"SELECT * FROM locations");
+                                    $result = mysqli_query($con, "SELECT * FROM locations");
                                     while ($row = mysqli_fetch_array($result)) {
 
-                                    echo "new L.Marker([" . $row['latitude'] . ", " . $row['longitude'] . "]),\n";
-                                    
+                                          echo "new L.Marker([" . $row['latitude'] . ", " . $row['longitude'] . "]),\n";
                                     } ?>
                               ]);
                               leafletMarkers.addTo(map);
