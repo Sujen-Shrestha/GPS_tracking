d0e02f6d64d7cfc2353ecceaef0c80e6900ef881
Sujen Shrestha <sujen@pop-os.localdomain>
2022-12-14 19:58:13 +0545

    user panel map fixed

--- main.php
+++ main.php
@@ -37,49 +37,106 @@ session_start();
       <div class="main-content">
             <?php include './user/header-user.php';?>
             <main>
-                  <div class="cards">
-                        <div class="card-single">
-                              <div>
-                                  <h1>Yes</h1>
-                                  <span>Schedule-today</span>  
+                  <div class="content homepage active">
+                        <div class="cards">
+                              <div class="card-single">
+                                    <div>
+                                    <h1>Yes</h1>
+                                    <span>Schedule-today</span>  
+                                    </div>
+                                    <div>
+                                          <span class="las la-calendar-day"></span>
+                                    </div>
                               </div>
-                              <div>
-                                    <span class="las la-calendar-day"></span>
+                              <div class="card-single">
+                                    <div>
+                                    <h1>Active</h1>
+                                    <span>Account-Status</span>  
+                                    </div>
+                                    <div>
+                                          <span class="las la-wallet"></span>
+                                    </div>
                               </div>
-                        </div>
-                        <div class="card-single">
-                              <div>
-                                  <h1>Active</h1>
-                                  <span>Account-Status</span>  
+                              <div class="card-single-time">
+                                    <div>
+                                    <h1>3</h1>
+                                    <span>Time-Remaining</span>  
+                                    </div>
+                                    <div>
+                                          <span class="las la-calendar-times"></span>
+                                    </div>
                               </div>
-                              <div>
-                                    <span class="las la-wallet"></span>
+                              <div class="card-single">
+                                    <div>
+                                    <h1>Gongabu</h1>
+                                    <span>My-Area</span>  
+                                    </div>
+                                    <div>
+                                          <span class="las la-map-marker"></span>
+                                    </div>
                               </div>
-                        </div>
-                        <div class="card-single-time">
-                              <div>
-                                  <h1>3</h1>
-                                  <span>Time-Remaining</span>  
-                              </div>
-                              <div>
-                                    <span class="las la-calendar-times"></span>
-                              </div>
-                        </div>
-                        <div class="card-single">
-                              <div>
-                                  <h1>Gongabu</h1>
-                                  <span>My-Area</span>  
-                              </div>
-                              <div>
-                                    <span class="las la-map-marker"></span>
-                              </div>
-                        </div>
 
+                        </div>  
+                  </div>
+                  <div class="content mappage in-active">
+                        <div id = "map" class="map-panel"></div>
+                        <script src = "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+                        <script>
+
+                              <?php
+                                    $con = mysqli_connect('localhost', 'sagar', 'Iamsagar456@');
+                                    mysqli_select_db($con, 'sagar');
+
+                                    $result = mysqli_query($con, "SELECT * FROM locations");
+                                    while ($row = mysqli_fetch_array($result)) {
+                                          $lat_d = $row['latitude'];
+                                          $long_d = $row['longitude'];
+                                    }
+
+                                    $result = array(array('latitude' => $lat_d, 'longitude' => $long_d));
+                                    // Set the latitude and longitude coordinates of the location
+                                    $latitude = $lat_d;
+                                    $longitude = $long_d;
+                              ?>
+                              const lat = <?php echo $latitude; ?>;
+                              const long = <?php echo $longitude; ?>;;
+                              // Creating map options
+
+                              var mapOptions = {
+                                    center: [lat, long],
+                                    zoom: 13
+                              }
+                              
+                              // Creating a map object
+                              var map = new L.map('map', mapOptions);
+                              
+                              // Creating a Layer object
+                              var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+                              
+                              // Adding layer to the map
+                              map.addLayer(layer);
+                              
+                              const leafletMarkers = L.layerGroup([
+
+
+                              //adding marker
+                              <?php
+
+                                    $result = mysqli_query($con,"SELECT * FROM locations");
+                                    while ($row = mysqli_fetch_array($result)) {
+
+                                    echo "new L.Marker([" . $row['latitude'] . ", " . $row['longitude'] . "]),\n";
+                                    
+                                    } ?>
+                              ]);
+                              leafletMarkers.addTo(map);
+                        </script>
                   </div>
 
 
             </main>
       </div>
 </body>
+
 
 </html>
